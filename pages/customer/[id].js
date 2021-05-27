00d9d227e52351a286723180b7340c8152d1aa10
prolific117 <olu.oduro@gmail.com>
2021-05-27 11:16:52 +0100

    customer details

--- pages/customer/[id].js
+++ pages/customer/[id].js
@@ -1,8 +1,11 @@
 const baseUrl = "https://fathomless-temple-12276.herokuapp.com/";
+import { useRouter } from 'next/router'
+import Button from 'react-bootstrap/Button';
+import React, {useEffect, useState } from "react";
 
 export default function Customer() {
   const navbarLinks = [
-    {name: "Customers", link: "/ho"},
+    {name: "Customers", link: "/customers"},
     {name: "Revenue", link: "/ho"},
     {name: "Integrations", link: "/integrations"}
   ]
@@ -25,6 +28,114 @@ export default function Customer() {
 
   }
 
+  const [customer, setCustomer] = useState('');
+  const [payments, setPayments] = useState('');
+  const [subscriptions, setSubscriptions] = useState('');
+  const [mandates, setMandates] = useState('');
+  
+  const router = useRouter();
+  let pid = router.query.id;
+
+  const getCustomerData = async event => {    
+    useEffect(() => {
+        const sessionId = localStorage.getItem("sessionId");  
+        if (sessionId == null) {
+            Router.push('/login')
+        }
+
+        //Get customer details
+        fetch("https://fathomless-temple-12276.herokuapp.com/customer/"+pid, {
+            method: "GET",
+            headers: {"Authorization": sessionId}
+        })
+        .then(response => Promise.all([response, response.json()]))
+        .then(([response, body]) => {
+            if (response.ok) {
+               setCustomer(body)
+               return
+            } else {
+               return response
+            }
+        })
+        .then(function(object) {
+            if(object != undefined) {
+                if(object.status === 401) {
+                   Router.push('/login')
+                }
+            }
+        });
+
+       //Get mandates
+       fetch("https://fathomless-temple-12276.herokuapp.com/gocardless/mandates/"+pid, {
+          method: "GET",
+          headers: {"Authorization": sessionId}
+          })
+          .then(response => Promise.all([response, response.json()]))
+          .then(([response, body]) => {
+              if (response.ok) {
+                console.log(body)
+                setMandates(body)
+                return
+              } else {
+                return response
+              }
+          })
+          .then(function(object) {
+              if(object != undefined) {
+                  if(object.status === 401) {
+                    Router.push('/login')
+                  }
+              }
+          });
+
+        //Get customer payments
+       fetch("https://fathomless-temple-12276.herokuapp.com/gocardless/payments/"+pid, {
+          method: "GET",
+          headers: {"Authorization": sessionId}
+          })
+          .then(response => Promise.all([response, response.json()]))
+          .then(([response, body]) => {
+              if (response.ok) {
+                setPayments(body)
+                return
+              } else {
+                return response
+              }
+          })
+          .then(function(object) {
+              if(object != undefined) {
+                  if(object.status === 401) {
+                    Router.push('/login')
+                  }
+              }
+          });
+
+          //Get customer subscriptions
+          fetch("https://fathomless-temple-12276.herokuapp.com/gocardless/subscriptions/"+pid, {
+            method: "GET",
+            headers: {"Authorization": sessionId}
+            })
+            .then(response => Promise.all([response, response.json()]))
+            .then(([response, body]) => {
+                if (response.ok) {
+                  setSubscriptions(body)
+                  console.log(body)
+                  return
+                } else {
+                  return response
+                }
+            })
+            .then(function(object) {
+                if(object != undefined) {
+                    if(object.status === 401) {
+                      Router.push('/login')
+                    }
+                }
+            });
+        }, []);
+   }
+
+  getCustomerData()
   return (
     <div style={{ backgroundColor: '#edf2f9', paddingBottom: '50px'}}>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -49,9 +160,12 @@ export default function Customer() {
           <div className="col-12">
             <div className="card px-0 mt-5" style={{border:0, boxShadow: '0 7px 14px 0 rgb(65 69 88 / 10%), 0 3px 6px 0 rgb(0 0 0 / 7%)', backgroundColor: '#f8f9fa'}}>
               <div className="card-body text-center">
-                <h5 className="card-title">Alistair Marshall <a href="#">(am@gc.com)</a></h5>
-                <h6 className="text-muted" style={{fontWeight: 300, fontSize: 13}}>Created on Feb 02, 2020</h6>
-                <h6 className="text-muted" style={{fontWeight: 300, fontSize: 13}}><strong>Total Orders:</strong> 21</h6>
+               {customer && (
+                <div>
+                  <h5 className="card-title">{customer.first_name} {customer.last_name} <a href="#">({customer.email})</a></h5>
+                  <h6 className="text-muted" style={{fontWeight: 300, fontSize: 13}}>Created on ({customer.create_date})</h6>
+                </div>
+                )}
               </div>
             </div>
           </div>
@@ -65,11 +179,13 @@ export default function Customer() {
                     <tbody>
                       <tr style={{ fontSize: '12px'}}>
                         <th scope="row" style={{ border: '0', fontSize: '13px', paddingTop: 0, paddingBottom: 0, paddingLeft: 0 }}>ID</th>
-                        <td style={{ border: '0', fontSize: '13px', paddingTop: 0, paddingBottom: 0}}>0329902</td>
+                        <td style={{ border: '0', fontSize: '13px', paddingTop: 0, paddingBottom: 0}}>
+                          {customer && (<span className="error"> {customer.id} </span>)}
+                          </td>
                       </tr>
                       <tr style={{ fontSize: '12px'}}>
                         <th scope="row" style={{ border: '0', fontSize: '13px', paddingTop: 0, paddingBottom: 0, paddingLeft: 0}}>Name</th>
-                        <td style={{ border: '0', fontSize: '13px', paddingTop: 0, paddingBottom: 0}}>Ali</td>
+                        <td style={{ border: '0', fontSize: '13px', paddingTop: 0, paddingBottom: 0}}>{customer && (<span className="error"> {customer.first_name} {customer.last_name}</span>)}</td>
                       </tr>
                       <tr style={{ fontSize: '13px'}}>
                         <th scope="row" style={{ border: '0', fontSize: '13px', paddingTop: 0, paddingBottom: 0, paddingLeft: 0}}>Language</th>
@@ -83,15 +199,15 @@ export default function Customer() {
                       <tbody>
                         <tr style={{ fontSize: '12px'}}>
                           <th scope="row" style={{ border: '0', fontSize: '13px', paddingTop: 0, paddingBottom: 0, paddingLeft: 0 }}>Billing Details</th>
-                          <td style={{ border: '0', fontSize: '13px', paddingTop: 0, paddingBottom: 0}}>12 Red Street, London, SE5 5SE</td>
+                          <td style={{ border: '0', fontSize: '13px', paddingTop: 0, paddingBottom: 0}}>{customer && (<span className="error"> {customer.address_line} </span>)}</td>
                         </tr>
                         <tr style={{ fontSize: '12px'}}>
                           <th scope="row" style={{ border: '0', fontSize: '13px', paddingTop: 0, paddingBottom: 0, paddingLeft: 0}}>Billing Email</th>
-                          <td style={{ border: '0', fontSize: '13px', paddingTop: 0, paddingBottom: 0}}>ali@gc.com</td>
+                          <td style={{ border: '0', fontSize: '13px', paddingTop: 0, paddingBottom: 0}}>{customer && (<span className="error"> {customer.email} </span>)}</td>
                         </tr>
                         <tr style={{ fontSize: '13px'}}>
                           <th scope="row" style={{ border: '0', fontSize: '13px', paddingTop: 0, paddingBottom: 0, paddingLeft: 0}}>Phone</th>
-                          <td style={{ border: '0', fontSize: '13px', paddingTop: 0, paddingBottom: 0}}>678 999 8212</td>
+                          <td style={{ border: '0', fontSize: '13px', paddingTop: 0, paddingBottom: 0}}>{customer && (<span className="error"> {customer.phone} </span>)}</td>
                         </tr>
                       </tbody>
                     </table>
@@ -105,7 +221,55 @@ export default function Customer() {
               <div className="card-body">
                 <h5 className="card-title">Payment Methods</h5>
                 <div className="jumbotron">
+
+                {subscriptions &&
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">Mandate</th>
+                      <th scope="col">Created At</th>
+                      <th scope="col">Scheme</th>
+                      <th scope="col">Status</th>
+                      <th></th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  
+                    {mandates.map((mandate, index) => {
+                      return (
+                        <tr>
+                          <td scope="col" style={{fontSize:'13px'}}>
+                            {mandate.mandate_id}
+                          </td>
+                          <td scope="col" style={{fontSize:'13px'}}>
+                            {mandate.created_at}
+                          </td>
+                          <td scope="col" style={{fontSize:'13px'}}>
+                           {mandate.scheme} 
+                          </td>
+                          <td scope="col" style={{fontSize:'13px'}}><a href="#">{mandate.status}</a></td>
+                          <td>
+                          <Button style={{fontSize:'13px'}} variant="primary">
+                                Add Payment
+                          </Button>
+                          </td>
+                          <td>
+                          <Button style={{fontSize:'13px'}} variant="primary">
+                               Add Subscription
+                          </Button>
+                          </td>
+                        </tr>
+                      )
+                    })}
+                  </tbody>
+                </table>
+                }
+
+               {!subscriptions &&
                   <p className="text-center">No payment methods linked to customer</p>
+               }
+
                   <div className="row justify-content-center">
                   <button onClick={addBankAccount} className="btn btn-info mx-2">Add bank account</button>
                   <button className="btn btn-info mx-2">Add card</button>
@@ -118,11 +282,13 @@ export default function Customer() {
           <div className="col-12">
             <div className="card px-0 mt-3" style={{border:0, boxShadow: '0 7px 14px 0 rgb(65 69 88 / 10%), 0 3px 6px 0 rgb(0 0 0 / 7%)', backgroundColor: '#f8f9fa'}}>
               <div className="card-body">
-                <h5 className="card-title">Orders</h5>
+                <h5 className="card-title">Payments</h5>
+                
+                
                 <table className="table">
                   <thead>
                     <tr>
-                      <th scope="col">ID</th>
+                      <th scope="col">Mandate</th>
                       <th scope="col">Amount</th>
                       <th scope="col">Products</th>
                       <th scope="col">Payment Method</th>
@@ -130,29 +296,74 @@ export default function Customer() {
                     </tr>
                   </thead>
                   <tbody>
-                    {[1344, 23435, 43550, 53561].map((i, index) => {
+                  
+                    {payments && payments.map((payment, index) => {
                       return (
                         <tr>
                           <td scope="col" style={{fontSize:'13px'}}>
-                            {i}
+                            {payment.mandate}
                           </td>
                           <td scope="col" style={{fontSize:'13px'}}>
-                            £24.99 {index !== 0 ? <span className="badge badge-success">Succeeded</span> : <span className="badge badge-danger">Failed</span> }
+                            £{payment.amount/100} 
                           </td>
                           <td scope="col" style={{fontSize:'13px'}}>
                             Amazing products
                           </td>
                           <td scope="col" style={{fontSize:'13px'}}><a href="#">Direct Debit</a></td>
-                          <td scope="col" style={{fontSize:'13px'}}>05/0{4-index}/2021</td>
+                          <td scope="col" style={{fontSize:'13px'}}>{payment.created_at}</td>
                         </tr>
                       )
                     })}
-                    <tr><a href="#">View all</a></tr>               
                   </tbody>
                 </table>
+                
               </div>
             </div>
           </div>
+
+          <div className="col-12">
+            <div className="card px-0 mt-3" style={{border:0, boxShadow: '0 7px 14px 0 rgb(65 69 88 / 10%), 0 3px 6px 0 rgb(0 0 0 / 7%)', backgroundColor: '#f8f9fa'}}>
+              <div className="card-body">
+                <h5 className="card-title">Subscriptions</h5>
+
+                 <table className="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">Mandate</th>
+                      <th scope="col">Interval</th>
+                      <th scope="col">Amount</th>
+                      <th scope="col">Start Date</th>
+                      <th scope="col">Next Payment Date</th>
+                      <th scope="col">Status</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  
+                    {subscriptions && subscriptions.map((subscription, index) => {
+                      return (
+                        <tr>
+                          <td scope="col" style={{fontSize:'13px'}}>
+                            {subscription.mandate}
+                          </td>
+                          <td scope="col" style={{fontSize:'13px'}}>
+                            {subscription.interval}
+                          </td>
+                          <td scope="col" style={{fontSize:'13px'}}>
+                           £{subscription.amount/100} 
+                          </td>
+                          <td scope="col" style={{fontSize:'13px'}}><a href="#">{subscription.start_date}</a></td>
+                          <td scope="col" style={{fontSize:'13px'}}><a href="#">{subscription.upcoming_payments[0].charge_date}</a></td>
+                          <td scope="col" style={{fontSize:'13px'}}>{subscription.status}</td>
+                        </tr>
+                      )
+                    })}
+                  </tbody>
+                </table>
+
+                </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
